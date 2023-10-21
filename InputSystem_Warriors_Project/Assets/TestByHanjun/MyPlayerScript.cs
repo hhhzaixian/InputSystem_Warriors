137d919f8e499a3da632e91cb0103af2d9c7a0f1
韩俊 <joegohan@tencent.com>
2023-10-21 18:12:35 +0800

    添加GetStart代码和场景

---
+++ InputSystem_Warriors_Project/Assets/TestByHanjun/MyPlayerScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MyPlayerScript : MonoBehaviour
+{
+    public void Fire(InputAction.CallbackContext context)
+    {
+        Debug.Log("Fire!");
+    }
+}
